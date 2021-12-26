285bc07b778c3b2d1cb9ddd96ad00b25c3f6924b
Yossi Solomon <yossi.solomon@forter.com>
2021-12-26 14:03:08 +0200

    Create an actual stream for events to trigger

--- fritz.js
+++ fritz.js
@@ -49,34 +49,35 @@ if (nconf.get('pagerduty:serviceKey')) {
     const pager = new PagerDuty(nconf.get('pagerduty:serviceKey')),
           alertCheckIntervalSecs = nconf.get('pagerduty:alertCheckIntervalSecs'),
           lostMessagesThreshold = nconf.get('pagerduty:lostMessagesThreshold');
-    forwarder.messageLossCounter
+    const messgeLossTotalsStream = forwarder.messageLossCounter
         .bufferTime(alertCheckIntervalSecs * 1000)
         .map(events => {
-            const totalMessagesLost = events.reduce((a, b) => a + b, 0)
+            events.reduce((a, b) => a + b, 0)
+        })
+    messgeLossTotalsStream.subscribe(total => {
+        const state = totalMessagesLost >= lostMessagesThreshold ? 'failed' : 'passed';
 
-            const state = totalMessagesLost >= lostMessagesThreshold ? 'failed' : 'passed';
+        const func = state === 'failed' ? 'error' : 'info';
+        logger[func]('Forward client state changed to',  state);
 
-            const func = state === 'failed' ? 'error' : 'info';
-            logger[func]('Forward client state changed to',  state);
-
-            const service = 'fritz message loss';
-            const incidentKey = hostname + ' ' + service;
-            const eventType = state === 'failed' ? 'trigger' : 'resolve';
-            pager.call({
-                incidentKey,
-                eventType,
-                details: {
-                    time: new Date().toTimeString(),
-                    vm_data,
-                    service,
-                    state,
-                    totalMessagesLost
-                },
-                description: hostname + ' fritz dropped over ' + lostMessagesThreshold + ' in the last ' +
-                    alertCheckIntervalSecs + ' secs (' + totalMessagesLost + ' lost messages). ' +
-                    'See Fritz doc\'s Alerts section - https://forter.atlassian.net/wiki/spaces/ENG/pages/7897784/Fritz+-+Riemann+Proxy+System+Overview'
-            });
+        const service = 'fritz message loss';
+        const incidentKey = hostname + ' ' + service;
+        const eventType = state === 'failed' ? 'trigger' : 'resolve';
+        pager.call({
+            incidentKey,
+            eventType,
+            details: {
+                time: new Date().toTimeString(),
+                vm_data,
+                service,
+                state,
+                totalMessagesLost
+            },
+            description: hostname + ' fritz dropped over ' + lostMessagesThreshold + ' in the last ' +
+                alertCheckIntervalSecs + ' secs (' + totalMessagesLost + ' lost messages). ' +
+                'See Fritz doc\'s Alerts section - https://forter.atlassian.net/wiki/spaces/ENG/pages/7897784/Fritz+-+Riemann+Proxy+System+Overview'
         });
+    });
 }
 
 for (const key of ['conf', 'listen', 'forward', 'log', 'pagerduty']) {
