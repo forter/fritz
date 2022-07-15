a2db999927492959a35663fe96fe02189439fa67
pgilad <gilad.peleg@forter.com>
2022-07-15 20:00:15 +0300

    Extract pagerduty alerts

--- fritz.js
+++ fritz.js
@@ -1,15 +1,12 @@
 const net = require("node:net");
-const PagerDuty = require("./lib/pagerduty");
-const { map, bufferTime } = require("rxjs/operators");
 const { nconf } = require("./lib/config");
 const { ForwardClient } = require("./lib/forward-client");
 const { Msg, Reader, serialize } = require("./lib/proto");
 const process = require("node:process");
 const { logger } = require("./lib/logger");
+const { handlePagerdutyAlerts } = require("./lib/pagerduty-alerts");
 
 const forward = nconf.get("forward");
-
-logger.debug(`Starting forward client from ${forward.host}:${forward.port}`);
 
 const forwarder = new ForwardClient(
   forward.host,
@@ -21,75 +18,17 @@ const forwarder = new ForwardClient(
   forward.flushTimeout
 );
 
-const handlePagerdutyAlerts = () => {
-  const pager = new PagerDuty(nconf.get("pagerduty:serviceKey"));
-  const alertCheckIntervalSecs = nconf.get("pagerduty:alertCheckIntervalSecs");
-  const lostMessagesThreshold = nconf.get("pagerduty:lostMessagesThreshold");
-  const vm_data = nconf.get("pagerduty:vm_data");
-  const hostname = vm_data["hostname"];
-
-  const messageLossTotalsStream = forwarder.messageLossCounter.pipe(
-    bufferTime(alertCheckIntervalSecs * 1000),
-    map((events) => events.reduce((a, b) => a + b, 0))
-  );
-
-  let lossState = "passed";
-  messageLossTotalsStream.subscribe((totalMessagesLost) => {
-    const newState = totalMessagesLost >= lostMessagesThreshold ? "failed" : "passed";
-
-    if (newState !== lossState) {
-      lossState = newState;
-      const func = lossState === "failed" ? "error" : "info";
-      logger[func](
-        "Forward client state is " +
-          lossState +
-          " (dropped " +
-          totalMessagesLost +
-          " in the last " +
-          alertCheckIntervalSecs +
-          "secs)"
-      );
-
-      const service = "fritz message loss";
-      const incidentKey = `${hostname} ${service}`;
-      const eventType = lossState === "failed" ? "trigger" : "resolve";
-      pager.call({
-        incidentKey,
-        eventType,
-        details: {
-          time: new Date().toTimeString(),
-          vm_data,
-          service,
-          lossState,
-          totalMessagesLost,
-        },
-        description:
-          hostname +
-          " fritz dropped over " +
-          lostMessagesThreshold +
-          " in the last " +
-          alertCheckIntervalSecs +
-          " secs (" +
-          totalMessagesLost +
-          " lost messages). " +
-          "See Fritz doc's Alerts section - https://forter.atlassian.net/wiki/spaces/ENG/pages/7897784/Fritz+-+Riemann+Proxy+System+Overview",
-      });
-    }
-  });
-};
-
 if (nconf.get("pagerduty:serviceKey")) {
-  handlePagerdutyAlerts();
+  handlePagerdutyAlerts(forwarder);
 }
 
 for (const key of ["conf", "listen", "forward", "log", "pagerduty"]) {
   logger.debug(`config.${key}: ${JSON.stringify(nconf.get(key))}`);
 }
 
-forwarder.connect();
-
 const maxMessageLength = nconf.get("listen:maxMessageLength");
 const OK = serialize(Msg.create({ ok: true }));
+
 const server = net.createServer((socket) => {
   const clientRepr = `${socket.remoteAddress}:${socket.remotePort}`;
   const reader = new Reader(maxMessageLength);
@@ -120,6 +59,9 @@ const server = net.createServer((socket) => {
 const init = () => {
   const listenHost = nconf.get("listen:host");
   const listenPort = nconf.get("listen:port");
+
+  forwarder.connect();
+
   server.listen(listenPort, listenHost, () => {
     logger.info(`Server listening on ${listenHost}:${listenPort}`);
   });
